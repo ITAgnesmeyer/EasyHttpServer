1259dea350ce1b548925a6240f43a10d209c1fe7
DCSItoDIGA <guido.agnesmeyer@itagnesmeyer.com>
2021-07-08 09:25:26 +0200

    Update Project

--- EasyHttpServer/HttpServer.cs
+++ EasyHttpServer/HttpServer.cs
@@ -79,14 +79,14 @@ namespace EasyHttpServer
 
         private void TaskAction(object o)
         {
-            HttpServer server = o as HttpServer;
+            var server = o as HttpServer;
             if (server == null)
                 throw new Exception("cannot resolve ServerObject!");
 
             ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, errors) => true;
             using (server.CurrentListener = new HttpListener())
             {
-                foreach (string url in server.ListenerUrls)
+                foreach (var url in server.ListenerUrls)
                 {
                     try
                     {
@@ -111,7 +111,7 @@ namespace EasyHttpServer
                 {
                     OnServerLog(new ServerStatusEventArgs("start server", 0));
                     server.CurrentListener.Start();
-                    foreach (string prefixUrl in server.CurrentListener.Prefixes)
+                    foreach (var prefixUrl in server.CurrentListener.Prefixes)
                     {
                         OnServerLog(new ServerStatusEventArgs("listen to:" + prefixUrl, 0));
                     }
@@ -130,18 +130,18 @@ namespace EasyHttpServer
                 {
                     while (LoopEnable)
                     {
-                        HttpListenerContext context = server.CurrentListener.GetContext();
-                        string absPath = context.Request.Url.LocalPath;
-                        bool found = CheckUrlAndGetPath(absPath, server.RootPath, server.IndexFiles, out var path);
+                        var context = server.CurrentListener.GetContext();
+                        var absPath = context.Request.Url.LocalPath;
+                        var found = CheckUrlAndGetPath(absPath, server.RootPath, server.IndexFiles, out var path);
 
 
-                        using (HttpListenerResponse response = context.Response)
+                        using (var response = context.Response)
                         {
                             OnServerLog(new ServerStatusEventArgs("request:" + context.Request.Url, 0));
                             //CORS
                             response.Headers.Add("Access-Control-Allow-Origin:*");
 
-                            ServerPreRequestHandlerEventArgs args = new ServerPreRequestHandlerEventArgs(context.Request);
+                            var args = new ServerPreRequestHandlerEventArgs(context.Request);
                             try
                             {
                                 try
@@ -170,7 +170,7 @@ namespace EasyHttpServer
 
                                 response.ContentType = args.MimeType;
                                 response.StatusCode = (int)args.ReturnStatus;
-                                byte[] bytes = Encoding.UTF8.GetBytes(args.ContentToSend);
+                                var bytes = Encoding.UTF8.GetBytes(args.ContentToSend);
                                 response.OutputStream.Write(bytes, 0, bytes.Length);
                                 response.Close();
                                 continue;
@@ -211,7 +211,7 @@ namespace EasyHttpServer
         {
             if (!found)
             {
-                string message = string.Format(server.NotFoundMessage, absPath);
+                var message = string.Format(server.NotFoundMessage, absPath);
                 SendNotFoundResponse(message, response, server.DefaultErrorResponseType);
                 OnServerLog(new ServerStatusEventArgs(message, response.StatusCode));
                 return true;
@@ -220,26 +220,26 @@ namespace EasyHttpServer
             response.StatusCode = (int) HttpStatusCode.OK;
             if (File.Exists(path))
             {
-                FileInfo fi = new FileInfo(path);
+                var fi = new FileInfo(path);
                 response.ContentType = fi.MimeTypeOrDefault();
                 try
                 {
-                    byte[] bytes = File.ReadAllBytes(path);
-                    using (Stream output = response.OutputStream)
+                    var bytes = File.ReadAllBytes(path);
+                    using (var output = response.OutputStream)
                     {
                         output.Write(bytes, 0, bytes.Length);
                     }
                 }
                 catch (Exception e)
                 {
-                    string message = string.Format(server.InternalServerErrorMessage, e.Message);
+                    var message = string.Format(server.InternalServerErrorMessage, e.Message);
                     SendInternalServerError(response, message, server.DefaultErrorResponseType);
                     OnServerError(new ServerStartErrorEventArgs(e));
                 }
             }
             else
             {
-                string message = string.Format(server.BadRequestMessage, absPath);
+                var message = string.Format(server.BadRequestMessage, absPath);
                 SendBadRequestResponse(response, message, server.DefaultErrorResponseType);
                 OnServerLog(new ServerStatusEventArgs(message, (int) HttpStatusCode.BadRequest));
             }
@@ -250,7 +250,7 @@ namespace EasyHttpServer
         private void SendBadRequestResponse(HttpListenerResponse response, string message, string contentType)
         {
             response.ContentType = contentType;
-            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            var bytes = Encoding.UTF8.GetBytes(message);
             response.OutputStream.Write(bytes, 0, bytes.Length);
             response.StatusCode = (int)HttpStatusCode.BadRequest;
         }
@@ -258,13 +258,13 @@ namespace EasyHttpServer
         {
             response.StatusCode = (int)HttpStatusCode.InternalServerError;
             response.ContentType = contentType;
-            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            var bytes = Encoding.UTF8.GetBytes(message);
             response.OutputStream.Write(bytes, 0, bytes.Length);
         }
 
         private void SendNotFoundResponse(string message, HttpListenerResponse response, string contentType)
         {
-            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            var bytes = Encoding.UTF8.GetBytes(message);
             response.ContentType = contentType;
             response.OutputStream.Write(bytes, 0, bytes.Length);
             response.StatusCode = (int)HttpStatusCode.NotFound;
@@ -280,10 +280,10 @@ namespace EasyHttpServer
                 absolutPath = "";
             if (absolutPath.StartsWith("/"))
                 absolutPath = absolutPath.Substring(1);
-            string replacedFile = absolutPath;
+            var replacedFile = absolutPath;
             if (string.IsNullOrEmpty(replacedFile))
             {
-                foreach (string indexFile in indexFiles)
+                foreach (var indexFile in indexFiles)
                 {
                     cleanedPath = Path.Combine(rootPath, indexFile);
                     if (File.Exists(cleanedPath))
